d5ea4968b96e13ffc866dcef116596d5d7de9b26
Dominik Spicher <dominik.spicher@inacta.ch>
2020-02-21 23:48:11 +0100

    completed course

--- week_1_problem_set.py
+++ week_1_problem_set.py
@@ -2,7 +2,7 @@ from IPython import embed
 
 
 def question_7():
-    encrypted = bytes.fromhex("6c73d5240a948c86981bc294814d")
+    encrypted = bytes.fromhex("09e1c5f70a65ac519458e7e53f36")
     msg1 = bytes("attack at dawn", encoding='ascii')
     otp = [encrypted[i] ^ msg1[i] for i in range(len(msg1))]
     msg2 = bytes("attack at dusk", encoding='ascii')
--- week_2_problem_set.py
+++ week_2_problem_set.py
@@ -11,7 +11,15 @@ def question_4():
                  "325032a9 c5e2364b"],
                 ["7b50baab 07640c3d",
                  "ac343a22 cea46d60"]]
-    for i, pattern in enumerate(patterns):
+    patterns2 = [["290b6e3a 39155d6f",
+                  "d6f491c5 b645c008", ],
+                 ["5f67abaf 5210722b",
+                  "bbe033c0 0bc9330e"],
+                 ["9d1a4f78 cb28d863",
+                  "75e5e3ea 773ec3e6"],
+                 ["7b50baab 07640c3d",
+                  "ac343a22 cea46d60"]]
+    for i, pattern in enumerate(patterns2):
         print(i+1)
         parts = list(map(lambda x: x.split(" "), pattern))
         left00 = bytes.fromhex(parts[0][0])
@@ -23,8 +31,11 @@ def question_8():
     msgs = ['To consider the resistance of an enciphering process to being broken we should assume that at same times the enemy knows everything but the key being used and to break it needs only discover the key from this information.',
             'In this letter I make some remarks on a general principle relevant to enciphering in general and my machine.',
             'We see immediately that one needs little information to begin to break down the process.',
-            'The most direct computation would be for the enemy to try all 2^r possible keys, one by one.']
-    print([len(bytes(msg, encoding='ascii')) for msg in msgs])
+            'The most direct computation would be for the enemy to try all 2^r possible keys, one by one.',
+            'The significance of this general conjecture, assuming its truth, is easy to see. It means that it may be feasible to design ciphers that are effectively unbreakable.',
+            'If qualified opinions incline to believe in the exponential conjecture, then I think we cannot afford not to make use of it.']
+    used = [msgs[4], msgs[1], msgs[3], msgs[5]]
+    print([128 - 16 - len(bytes(msg, encoding='ascii')) for msg in used])
 
 
 question_4()
